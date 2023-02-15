bf98b445d21047ebd1b6dcd9a1f407bf948066e3
ClG <cg@jmg-conseil.eu>
2023-02-15 10:58:39 +0100

    cleaned packages

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ async function main() {
     const fs = await import('fs')
 
     const   filesDir    = 'src/lib/files',
-        files       = fs.readdirSync(filesDir),
-        gateway     = 'https://ipfs.io/ipfs/';
+            files       = fs.readdirSync(filesDir),
+            gateway     = 'https://ipfs.io/ipfs/';
 
     const { create } = await import('ipfs-core');
     const ipfs = await create(); // Create an IPFS node instance
 
     for(let file of files) {
-        const result = await ipfs.add(file)
+        const buffer = fs.readFileSync(`${filesDir}/${file}`)
+        const result = await ipfs.add(buffer)
+        console.log(`${file} uploaded to IPFS: ${gateway}${result.path}`)
     }
 }
 
