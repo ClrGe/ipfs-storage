fc08bc89737c06896ccb3c362528b262644cf4f9
ClG <cg@jmg-conseil.eu>
2023-02-15 17:20:20 +0100

    add to ipfs works

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,52 @@
 const express = require('express');
 const multer = require('multer');
+const cors = require('cors');
+const fs = require('fs');
+
+// Create a new express application instance
 
 const app = express();
 const upload = multer({ dest: 'lib/files' });
 
 let PORT = 3000;
 
-interface File {
-    filename: string;
-    data: Buffer;
-    size: number;
+const corsOptions = {
+    origin: 'http://localhost:5173'
 }
 
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }
+));
 // Start the express js server
 async function startServer() {
+    app.post('/upload', upload.single('filepond'), async (req: any, res: any) => {
+        if (!req.file) {
+            res.status(400).send('No file uploaded.');
+            return;
+        }
+        try {
+            console.log(`Received file: ${req.file.path}`);
+            await loadToIpfs(req.file);
+            return res.status(200).send('File uploaded to IPFS');
+        } catch (error) {
+            console.error(`Error uploading file to IPFS: ${error}`);
+            res.status(500).send(`Error uploading file to IPFS: ${error}`);
+            return res.status(500).send(`Error uploading file to IPFS: ${error}`);
+        }
 
-    app.post('/upload', async (req: any, res: any) => {
+    });
+
+    app.post('/test', cors(corsOptions), async (req: any, res: any) => {
         if (!req.files || Object.keys(req.files).length === 0) {
+            console.log('No files were uploaded.' + req.files);
             return res.status(400).send('No files were uploaded.');
         }
 
         const url = await loadToIpfs(req.files.file);
+        console.log(`File uploaded to IPFS: ${url}`);
         res.send(`File uploaded to IPFS: ${url}`);
     });
 
@@ -30,18 +56,21 @@ async function startServer() {
     });
 }
 
-// Upload files to IPFS
-async function loadToIpfs(file:File) {
+async function loadToIpfs(file: any) {
+    const fs = require('fs');
     const { create } = await import('ipfs-core');
-    const ipfs = await create(); // Create an IPFS node instance
-    const buffer = file.data;
-    const result = await ipfs.add(buffer);
+
+    if (!fs.statSync(file.path).isFile()) {
+        console.error(`Error uploading file to IPFS: '${file.path}' is not a file`);
+        return;
+    }
+
     const gateway = 'https://ipfs.io/ipfs/';
-    const url = `${gateway}${result.path}`;
+    const ipfs = await create(); // Create an IPFS node instance
 
-    console.log(`File uploaded to IPFS: ${url}`);
-
-    return url;
+    const buffer = fs.readFileSync(file.path);
+    const result = await ipfs.add(buffer);
+    console.log(`'${file.originalname}' uploaded to IPFS: ${gateway}${result.path}`);
 }
 
 startServer();
