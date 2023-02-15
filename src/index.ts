0e5d1601e2a19dc5d447dd6a082fc5690e72376f
ClG <cg@jmg-conseil.eu>
2023-02-15 14:02:10 +0100

    implement express server with POST /upload

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,47 @@
-async function main() {
+const express = require('express');
+const multer = require('multer');
 
-    const fs = await import('fs')
+const app = express();
+const upload = multer({ dest: 'lib/files' });
 
-    const   filesDir    = 'src/lib/files',
-            files       = fs.readdirSync(filesDir),
-            gateway     = 'https://ipfs.io/ipfs/';
+let PORT = 3000;
 
-    const { create } = await import('ipfs-core');
-    const ipfs = await create(); // Create an IPFS node instance
-
-    for(let file of files) {
-        const buffer = fs.readFileSync(`${filesDir}/${file}`)
-        const result = await ipfs.add(buffer)
-        console.log(`${file} uploaded to IPFS: ${gateway}${result.path}`)
-    }
+interface File {
+    filename: string;
+    data: Buffer;
+    size: number;
 }
 
-main()
+// Start the express js server
+async function startServer() {
+
+    app.post('/upload', async (req: any, res: any) => {
+        if (!req.files || Object.keys(req.files).length === 0) {
+            return res.status(400).send('No files were uploaded.');
+        }
+
+        const url = await loadToIpfs(req.files.file);
+        res.send(`File uploaded to IPFS: ${url}`);
+    });
+
+
+    app.listen(PORT, () => {
+        console.log('Server listening on port '+ PORT +'.');
+    });
+}
+
+// Upload files to IPFS
+async function loadToIpfs(file:File) {
+    const { create } = await import('ipfs-core');
+    const ipfs = await create(); // Create an IPFS node instance
+    const buffer = file.data;
+    const result = await ipfs.add(buffer);
+    const gateway = 'https://ipfs.io/ipfs/';
+    const url = `${gateway}${result.path}`;
+
+    console.log(`File uploaded to IPFS: ${url}`);
+
+    return url;
+}
+
+startServer();
