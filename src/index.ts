5d2e1e34c874684d5543c60a311c3386989a5ae3
ClG <cg@jmg-conseil.eu>
2023-02-16 16:01:03 +0100

    register

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,39 @@
 // ------- Back-end pour la plateforme de dépôt de ressources numériques vers divers supports --------
 // ---------------------------------------------------------------------------------------------------
 
+
+require('dotenv').config();
+
+// -----------------
+// --- Variables ---
+// -----------------
+
 const   PORT            = process.env.PORT,
         MONGO_URI       = process.env.MONGO_URI,
         MONGO_BASE      = process.env.MONGO_BASE,
         GATEWAY         = process.env.IPFS_GATEWAY,
+        SMTP_SERVER     = process.env.SMTP_SERVER,
+        SMTP_PORT       = process.env.SMTP_PORT,
+        SMTP_USER       = process.env.SMTP_USER,
+        SMTP_PASSWORD   = process.env.SMTP_PASSWORD,
+
         cors            = require('cors'),
         fs              = require('fs'),
         path            = require('path'),
         bcrypt          = require('bcrypt'),
+        nodemailer      = require('nodemailer'),
         { MongoClient } = require('mongodb'),
-        client          = new MongoClient(MONGO_URI),
         multer          = require('multer'),
-        upload          = multer({ dest: 'lib/files/' }),
-        express         = require('express'),
-        app             = express();
+        express         = require('express');
 
-const db = client.db('depot');
 
-let collection,
-    log: string;
+const   app     = express(),
+        client  = new MongoClient(MONGO_URI),
+        db      = client.db(MONGO_BASE),
+        upload  = multer({ dest: 'lib/files/' });
+
+let     collection,
+        log: string;
 
 // -----------------
 // --- Middleware ---
@@ -34,6 +48,9 @@ app.use(cors(
     }
 ));
 
+app.use(express.json())
+app.use(cors())
+
 // -----------------
 // --- Endpoints ---
 // -----------------
@@ -45,25 +62,33 @@ async function startServer() {
     // endpoint to register a new user
     app.post('/register', async (req: any, res: any) => {
         let collection = db.collection('users');
-        const user = req.body;
+        console.log(req.body)
         let result = await collection.insertOne(
-            {_id : 'todto', user:  {user}})
+            {
+                email: req.body.email,
+                password: req.body.password,
+                date: new Date(),
+             },
+        );
 
         if (result) {
-            log = `New user registered: ${user.email}`;
+            log = `New user registered: `;
             await saveTraces(201, log, 'POST /register');
             res.status(201).send('ok');
         } else {
-            log = `Error registering new user: ${user.email}`;
+            log = `Error registering new user: `;
             await saveTraces(500, log, 'POST /register');
             res.status(500).send('error');
         }
     });
 
-    // endpoint to login a user
+    // endpoint to log a user in
     app.post('/login' , async (req: any, res: any) => {
-        collection = db.collection('users');
-        log = `Login attempt from ${req.body.email} `;
+        let collection = db.collection('users');
+        console.log(req.body)
+        log = `Login attempt from ${req.body} `;
+        let result = await collection.findOne({"email": req.body.email, "password": req.body.password});
+
         console.log(log);
         await saveTraces(200, log, 'POST /login');
 
@@ -92,12 +117,10 @@ async function startServer() {
         }
     });
 
-
     // endpoint to upload a file and send it to ipfs
     app.post('/upload', upload.single('filepond'), async (req: any, res: any) => {
         if (!req.file) {
             log = 'Failed - No file uploaded.';
-
             res.status(400).send('No file uploaded.');
             return;
         }
@@ -116,10 +139,20 @@ async function startServer() {
     // endpoint to send the list of transactions
     app.get('/transactions', async (req: any, res: any) => {
         let collection = db.collection('transactions');
-        const transactions = await collection.find().toArray();
-        log = `Transactions sent: ${transactions}`;
-        await saveTraces(200, log, 'GET /transactions');
-        res.status(200).send(transactions);
+
+        try {
+            const transactions = await collection.find().toArray();
+            log = `Transactions sent: ${transactions}`;
+            await saveTraces(200, log, 'GET /transactions');
+            console.log(log)
+            res.status(200).send(transactions);
+        } catch (error) {
+            log = `Error sending transactions: ${error}`;
+            console.error(log);
+            await saveTraces(500, log, 'GET /transactions');
+            res.status(500).send(`Error sending transactions: ${error}`);
+        }
+
     });
 
     // start server
@@ -128,33 +161,48 @@ async function startServer() {
     });
 }
 
-
 // -----------------
 // --- Functions ---
 // -----------------
 
-
-
 async function hashPassword(password: string){
-    const   saltRounds  = 10,
-            salt        = bcrypt.genSaltSync(saltRounds);
-
-    return bcrypt.hashSync(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
 }
 
-// upload to IPFS directly from POST request
-async function uploadToIpfs(file: any) {
-    let {create} = await import('ipfs-core');
+// send email to user from POST request
+async function sendEmail(subject: string, message: string, to: string) {
+    // use nodemailer to send email
+    const transporter = nodemailer.createTransport({
+        host: SMTP_SERVER,
+        port: SMTP_PORT,
+        secure: false,
+        auth: {
+            user: SMTP_USER,
+            pass: SMTP_PASSWORD
+        },
+        tls: {
+            rejectUnauthorized: false,
+            servername: 'smtp.ethereal.email'
+        }
+    });
 
-    const   ipfs    = await create(), // Create an IPFS node instance
-            result  = await ipfs.add(file);
+    const email = {
+        from: SMTP_USER,
+        to: to,
+        subject: subject,
+        text: message
+    }
 
-    log = `'${file.originalname}' uploaded to IPFS: ${GATEWAY}${result.path}`;
 
-    await db.collection('transactions').insertOne({
-        name: file.originalname,
-        hash: result.path,
-        address: GATEWAY + result.path
+    transporter.sendMail(email, (err: any, info: any) => {
+        if (err) {
+            log = `Error sending email: ${err}`;
+            console.error(log);
+            return;
+        }
+        log = `Email sent: ${info.response}`;
+        console.log(log);
     });
 
 }
@@ -182,18 +230,18 @@ async function loadToIpfs(file: any) {
         return;
     }
 
-    const   gateway = 'https://ipfs.io/ipfs/',
-            ipfs = await create(), // Create an IPFS node instance
+    const   ipfs = await create(), // Create an IPFS node instance
             buffer = fs.readFileSync(file.path),
             result = await ipfs.add(buffer);
 
-    log = `'${file.originalname}' uploaded to IPFS: ${gateway}${result.path}`;
+    log = `'${file.originalname}' uploaded to IPFS: ${GATEWAY}${result.path}`;
     console.log(log);
 
     await db.collection('transactions').insertOne({
         name: file.originalname,
         hash: result.path,
-        address: gateway + result.path
+        address: GATEWAY + result.path,
+        date: new Date()
     });
 
 }
